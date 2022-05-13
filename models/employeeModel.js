810dfcbcec0156507a7e42f22f3f48cf0230c8a7
jaco van der merwe <jacovandermerwe97@gmail.com>
2022-05-13 19:09:24 +0200

    add random id

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const employeeSchema =  new mongoose.Schema({
+    ID: {
+        type: String
+    },
     basicInformation: {
         firstName: {
             type: String,
